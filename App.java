05c4aa66c7785e3e22227643f9a8011a82717362
Prof. Daniel Callegari <43687162+profdcallegari@users.noreply.github.com>
2020-09-04 18:40:34 -0300

    Update App.java

--- App.java
+++ App.java
@@ -1,13 +1,6 @@
-public class App
-{
-  public static void main (String[] args)
-  {
-  
+public class App {
+  public static void main (String[] args) {
     System.out.println("Olá, turma de GCS.");
-  
-  
+    System.out.println("Até breve!");
   }
-
-
-
 }
