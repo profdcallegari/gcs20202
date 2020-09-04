f546db395ee9d4ca48ac494a939bf6967e003eac
Saccilotto <42774909+Saccilotto@users.noreply.github.com>
2020-09-04 18:46:27 -0300

    Update App.java
    Teste para ver se eh possível criar sugestões para open source no github a partir do comando fork.

--- App.java
+++ App.java
@@ -1,6 +1,11 @@
+import java.util.Scanner;
 public class App {
   public static void main (String[] args) {
+    Scanner input = new Scanner(System.in);
     System.out.println("Olá, turma de GCS.");
-    System.out.println("Até breve!");
+    System.out.println("Apresente-se para turma!")
+    String aux = input.next(); 
+    System.out.println("Até breve." + aux + "!");
+    
   }
 }
