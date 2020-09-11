f197ba71a40cc8494ae07b5c50216ffb129c7d6b
Daniel Callegari <daniel.callegari@pucrs.br>
2020-09-11 18:44:32 -0300

    Praticando git

--- App.java
+++ App.java
@@ -1,6 +1,9 @@
 public class App {
   public static void main (String[] args) {
     System.out.println("Olá, turma de GCS.");
+	
+	System.out.println("Estamos hoje praticando git.");
+	
     System.out.println("Até breve!");
   }
 }
